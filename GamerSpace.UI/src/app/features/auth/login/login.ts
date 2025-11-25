4d695f880fd260926498441b5ff12e8d7ecc9c8f
Dérick Pipoca <derickwfsilva@gmail.com>
2025-11-25 16:18:28 -0300

    feat: redesign login page with improved layout, validation messages, and social login options

--- GamerSpace.UI/src/app/features/auth/login/login.ts
+++ GamerSpace.UI/src/app/features/auth/login/login.ts
@@ -2,12 +2,15 @@ import { AuthService } from './../../../core/services/auth-service';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FaIconLibrary, FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faApple, faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FaIconComponent, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
@@ -18,7 +21,10 @@ export class Login implements OnInit {
   constructor(
     private authService: AuthService,
     private router: Router,
-  ) {}
+    lib: FaIconLibrary,
+  ) {
+    lib.addIcons(faUser, faLock, faFacebook, faApple, faGoogle);
+  }
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
@@ -40,6 +46,8 @@ export class Login implements OnInit {
     this.authService.login(loginDto).subscribe({
       next: (response) => {
         console.log('Login bem-sucedido!', response.token);
+        alert('Login bem-sucedido!');
+        this.router.navigate(['home']);
       },
       error: (err) => {
         this.error = err.error.detail || 'Falha no login.';
